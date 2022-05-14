caf453cd43d48bca3d8246cb5422e2f13ea493f2
Sweets Sweetman <sweetmantech@gmail.com>
2022-05-14 16:12:07 -0300

    I abstract most of pages/index.js into <HomePage /> component.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import ButtonCreateERC721 from "../src/components/ButtonCreateERC721";
+import HomePage from "../src/components/HomePage";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -13,51 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hyperstructures</h1>
-        <p>(on Polygon & ETH)</p>
-
-        <p className={styles.description}>
-          Get started by deploying your own smart contract.
-        </p>
-        <ButtonCreateERC721 />
-
-        <div className={styles.grid}>
-          <a
-            href="https://faucet.polygon.technology/"
-            className={styles.card}
-            target="__blank"
-          >
-            <h2>Polygon Faucet &rarr;</h2>
-            <p>Get free Mumbai MATIC.</p>
-          </a>
-
-          <a
-            href="https://rinkebyfaucet.com/"
-            target="__blank"
-            className={styles.card}
-          >
-            <h2>Rinkeby Faucet</h2>
-            <p>Get free Rinkeby ETH.</p>
-          </a>
-
-          <a
-            href="https://github.com/SweetmanTech/next-contract-deployer"
-            target="__blank"
-            className={styles.card}
-          >
-            <h2>Open Source</h2>
-            <p>View code on Github.</p>
-          </a>
-
-          <a
-            href="https://jacob.energy/hyperstructures.html"
-            target="__blank"
-            className={styles.card}
-          >
-            <h2>Hyperstructures</h2>
-            <p>Learn more.</p>
-          </a>
-        </div>
+        <HomePage />
       </main>
 
       <footer className={styles.footer}>
