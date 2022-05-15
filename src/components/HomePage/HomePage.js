eb9f9b3ca99181324e7e426778ac89956f1d64f7
Ronald <ronaldpelham51@outlook.com>
2022-05-15 14:49:55 -0300

    I update demo text.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,9 @@
-import ButtonCreateERC721 from "../ButtonCreateERC721";
 import styles from "../../../styles/Home.module.css";
 import MintNFT from "../MintNFT";
 import { useState } from "react";
 import { Box } from "@mui/system";
 import CreateERC721 from "../CreateERC721";
+import { Typography } from "@mui/material";
 
 const HomePage = () => {
   const [contractAddress, setContractAddress] = useState();
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <>
       <h1 className={styles.title}>Hyperstructures</h1>
-      <p>(on Polygon & ETH)</p>
+      <p>for music nfts (on Polygon & ETH)</p>
 
       {contractAddress ? (
         <MintNFT contractAddress={contractAddress} />
@@ -23,10 +23,11 @@ const HomePage = () => {
             alignItems: "center",
           }}
           p={3}
+          gap={3}
         >
-          <p className={styles.description}>
-            Get started by deploying your own smart contract.
-          </p>
+          <Typography variant="h5">
+            Get started: create your ERC721 smart contract.
+          </Typography>
           <CreateERC721 onDeployed={setContractAddress} />
         </Box>
       )}
