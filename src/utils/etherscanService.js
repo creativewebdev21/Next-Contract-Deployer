3b5cb37599c0be7a19cafaf04537d9afb48c4521
Ronald <ronaldpelham51@outlook.com>
2022-05-15 15:55:23 -0300

    I display etherscan links for each chain.

---
+++ src/utils/etherscanService.js
@@ -0,0 +1,17 @@
+const getAddressLink = (chainId, address) => {
+  console.log("GETTING ADDRESS", address);
+  switch (chainId) {
+    case "0x4":
+      return `https://rinkeby.etherscan.io/address/${address}`;
+    case "0x13881":
+      return `https://mumbai.polygonscan.com/address/${address}`;
+    case "0x89":
+      return `https://polygonscan.com/address/${address}`;
+    case "0x1":
+      return `https://etherscan.io/address/${address}`;
+  }
+};
+
+const etherscanService = { getAddressLink };
+
+export default etherscanService;
