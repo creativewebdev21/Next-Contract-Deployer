b78e789fa6d424afdaa7050624de40d1d4756566
Ronald <ronaldpelham51@outlook.com>
2022-05-14 21:00:05 -0300

    I init OpenSeaService.getTokenLink util.

--- src/components/ConfirmedTxModal/ConfirmedTxModal.js
+++ src/components/ConfirmedTxModal/ConfirmedTxModal.js
@@ -1,22 +1,27 @@
 import { Button, Typography } from "@mui/material";
+import { useConnectWallet } from "@web3-onboard/react";
 import { useEffect, useState } from "react";
+import openSeaService from "../../utils/openSeaService";
 import Modal from "../Modal";
 
 const ConfirmedTxModal = ({ tokenId, contractAddress }) => {
+  const [{ wallet }] = useConnectWallet();
   const [open, setOpen] = useState();
 
   useEffect(() => {
     setOpen(contractAddress, tokenId);
   }, [contractAddress, tokenId]);
 
+  const osLink = openSeaService.getTokenLink(
+    wallet.chains[0].id,
+    contractAddress,
+    tokenId
+  );
+
   return (
     <Modal open={open} handleClose={() => setOpen(false)}>
       <Typography variant="h3">NFT Minted!</Typography>
-      <Button
-        variant="contained"
-        target="__blank"
-        href={`https://testnets.opensea.io/assets/${contractAddress}/${tokenId}`}
-      >
+      <Button variant="contained" target="__blank" href={osLink}>
         View on OpenSea
       </Button>
     </Modal>
